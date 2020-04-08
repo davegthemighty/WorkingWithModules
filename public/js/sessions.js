9b3f320c5699ea3f6b5837f26b90e7d34ecf249d
DavegTheMighty <dave@example.com>
2020-04-08 12:59:17 +0100

    Switch to ES2015 modules

---
+++ public/js/sessions.js
@@ -0,0 +1,23 @@
+const sessionsURL = 'sessions.json';
+
+let sessionList = [];
+
+function getSessions(){
+  return new Promise(function(resolve, reject){
+    var oReq = new XMLHttpRequest();
+    oReq.onload = function (e) {
+      sessionList = e.target.response;
+      resolve(e.target.response);
+    };
+    oReq.open('GET', sessionsURL, true);
+    oReq.responseType = 'json';
+    oReq.send();
+  })
+}
+
+export {
+   getSessions as default, 
+   sessionsURL
+};
+
+export { sessionTemplate } from './template.js'
